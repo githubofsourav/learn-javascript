593c0d082997e958d0477bee17f0397d9dcec741
Sourav Das <sourav.das@nagarro.com>
2024-09-12 10:46:37 +0530

    updated callbacks example

--- Namaste_Javascript_02/Understanding_CallBacks/callbacks.js
+++ Namaste_Javascript_02/Understanding_CallBacks/callbacks.js
@@ -14,32 +14,56 @@ function processPayment(orderId) {
 }
 
 const cart = ["shoe", "shirt"];
-const orderId = createOrder(cart); // Block until order is created
+const orderId1 = createOrder(cart); // Block until order is created
 processPayment(orderId); // Proceed to payment after order is created
 
 // *****WITH CALLBACKS*****
-function createOrder2(cart, callback) {
-    console.log("Creating order...");
-  
-    // Simulating async operation with setTimeout (as if a DB operation happens)
-    setTimeout(() => {
-      const orderId = "12345";
-      console.log("Order created with ID:", orderId);
-      
-      // Once the order is created, we call the callback to handle the next step (payment)
-      callback(orderId);
-    }, 2000); // Simulates a 2-second delay
+
+// createOrder
+function createOrder2(cart, doPayment) {
+  // some db ops
+  console.log("Creating order...");
+
+  let orderId;
+  // Simulating async operation with setTimeout (as if a DB operation happens)
+  setTimeout(function () {
+    if (cart.length) orderId = "12345";
+    console.log("OrderId created");
+    // Once the order is created, we call the callback to handle the next step (payment)
+    doPayment(orderId);
+  }, 3000);
+  return orderId;
+}
+
+// processPayment
+function processPayment2(orderId, getOrderSummary) {
+  console.log("Processing payment..");
+  let paymentStatus;
+  if (orderId) {
+    setTimeout(function () {
+      paymentStatus = true;
+      console.log("Payment successful!");
+      getOrderSummary(paymentStatus);
+    }, 3000);
+  } else {
+    paymentStatus = false;
+    console.log("failed to process payment");
   }
-  
-  function processPayment2(orderId) {
-    console.log("Processing payment for Order ID:", orderId);
+  return paymentStatus;
+}
+// orderSummary
+function orderSummary2(paymentStatus) {
+  if (paymentStatus) {
+    console.log("Order summary: ", cart);
+  } else {
+    console.log("Order summary: Fail");
   }
-  
-  const cart2 = ["shoe", "shirt"];
-  
-  // Using a callback to handle what happens after order creation
-  createOrder2(cart, function(orderId) {
-    processPayment2(orderId);  // This only runs after the order is created
+}
+
+// Using a callback to handle what happens after order creation
+const orderId2 = createOrder2(cart, function (orderId) {
+  processPayment2(orderId, function (paymentStatus) {
+    // This only runs after the order is created
+    orderSummary2(paymentStatus);
   });
-  
-  
+});
